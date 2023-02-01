3a4453e6bb8cbedfbfa174cbc53561e37f3900bb
FBosso <francesco.bosso@mail.polimi.it>
2023-02-01 12:11:05 +0100

    missing logos fixed

---
+++ _nuxt/static/1675249815/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fportfolio","\u002Fabout-mapflap","\u002Fjust-because","\u002Fgeospatial","\u002Fmachine-learning","\u002Fmachine-learning\u002F6","\u002Fmachine-learning\u002F5","\u002Fgeospatial\u002F2","\u002Fjust-because\u002F4","\u002Fgeospatial\u002F1","\u002Fgeospatial\u002F3","\u002Fmachine-learning\u002F7"]})
